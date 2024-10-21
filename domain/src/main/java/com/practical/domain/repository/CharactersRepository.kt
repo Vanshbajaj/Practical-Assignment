fbe71008b6b350f450800dbe6a27a330d0d42fdc
vbajaj <vbajaj55@gmail.com>
2024-10-21 17:24:57 +0530

    Removed the UseCases And Repo

--- domain/src/main/java/com/practical/domain/repository/CharactersRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.practical.domain.repository
-
-import com.domain.graphql.CharactersQuery
-import kotlinx.coroutines.flow.Flow
-
-interface CharactersRepository {
-    suspend fun getCharacters(): Flow<List<CharactersQuery.Data>>
-}
