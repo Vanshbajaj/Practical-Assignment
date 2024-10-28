7e83eed4523f0d1361c36b1cc46bd012871aa1b0
vbajaj <vbajaj55@gmail.com>
2024-10-28 16:48:54 +0530

    Added HttpLoggingInterceptor

--- core/src/main/java/com/practical/core/Apollo.kt
+++ core/src/main/java/com/practical/core/Apollo.kt
@@ -1,18 +1,27 @@
 package com.practical.core
 
 import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.network.okHttpClient
 import com.practical.common.Constants
 import dagger.Module
 import dagger.Provides
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import javax.inject.Singleton
 @Module
 object Apollo {
     @Provides
     @Singleton
     fun provideApolloClient(): ApolloClient {
+        val httpClient = OkHttpClient.Builder()
+            .addInterceptor(HttpLoggingInterceptor().apply {
+                level = HttpLoggingInterceptor.Level.BODY
+            })
+            .build()
+
         return ApolloClient.Builder()
             .serverUrl(Constants.RICK_MORTY_URL)
+            .okHttpClient(httpClient) // Attach the OkHttpClient to Apollo
             .build()
     }
-
 }
