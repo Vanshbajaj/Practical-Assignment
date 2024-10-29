63c8cce9d239953c56e5a43ae9f5b5c3fd4e4c3c
vbajaj <vbajaj55@gmail.com>
2024-10-29 10:46:18 +0530

    Update Linter

--- app/src/test/java/com/practical/assignment/ExampleUnitTest.kt
+++ app/src/test/java/com/practical/assignment/ExampleUnitTest.kt
@@ -1,8 +1,9 @@
 package com.practical.assignment
 
+import junit.framework.TestCase.assertEquals
 import org.junit.Test
 
-import org.junit.Assert.*
+
 
 /**
  * Example local unit test, which will execute on the development machine (host).
