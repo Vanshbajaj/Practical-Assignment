1a7a613a4009b5268b21110d6397cc2a963cdb55
vbajaj <vbajaj55@gmail.com>
2024-10-29 11:06:49 +0530

    Update Linter

--- domain/src/main/java/com/practical/domain/repository/CharacterRepository.kt
+++ domain/src/main/java/com/practical/domain/repository/CharacterRepository.kt
@@ -2,7 +2,6 @@ package com.practical.domain.repository
 
 import com.practical.domain.CharacterDataClass
 
-
 interface CharacterRepository {
     suspend fun getCharacters(): List<CharacterDataClass>
 }
