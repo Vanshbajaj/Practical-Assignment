c8972d6c823dce3f25ac9450bdeee9639e999046
vbajaj <vbajaj55@gmail.com>
2024-11-15 17:26:37 +0530

    Added Test Cases For Repository

--- data/src/test/java/com/practical/data/CharacterRepositoryImplTest.kt
+++ data/src/test/java/com/practical/data/CharacterRepositoryImplTest.kt
@@ -159,7 +159,6 @@ class CharacterRepositoryImplTest {
         }
 
     /******************************   get Character By Id  ***************************************************/
-    @SuppressWarnings("LongMethod")
     @Test
     fun `character data for when ApolloClient query is successful`() = runTest {
         // Given
