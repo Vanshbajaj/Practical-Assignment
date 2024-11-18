0f9164089d76edaf4dda7c9a023ff0a8db8ead58
vbajaj <vbajaj55@gmail.com>
2024-11-18 13:31:02 +0530

    Added Test Cases For Domain Layer and Added Dependencies

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -44,6 +44,7 @@ dependencies {
     implementation(libs.kotlinx.coroutines.core)
     implementation(libs.kotlinx.coroutines.android)
     testImplementation(libs.androidx.core.testing)
+    testImplementation(libs.kotlinx.coroutines.test)
     testImplementation(libs.turbine)
     testImplementation(libs.mockk)
 }
