7d8656bfcd390d4fc567e8cd715221c16255f1ff
vbajaj <vbajaj55@gmail.com>
2024-11-18 14:02:46 +0530

    Added Test Cases For ViewModel

--- presentation/src/main/java/com/practical/presentation/viewmodel/CharacterDetailsViewModel.kt
+++ presentation/src/main/java/com/practical/presentation/viewmodel/CharacterDetailsViewModel.kt
@@ -1,38 +1,27 @@
 package com.practical.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
-import com.practical.data.network.NetworkException
 import com.practical.domain.CharacterModel
-import com.practical.domain.usecases.GetCharacterByIdUseCase
-import com.practical.presentation.UiState
+import com.practical.domain.ResultState
+import com.practical.domain.usecases.GetCharacterUseCase
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.catch
 import javax.inject.Inject
 
 class CharacterDetailsViewModel @Inject constructor(
-    private val getCharacterByIdUseCase: GetCharacterByIdUseCase,
+    private val getCharacterUseCase: GetCharacterUseCase,
 ) : ViewModel() {
     private var characterId: String? = null
-    private val _characterState = MutableStateFlow<UiState<CharacterModel>>(UiState.Loading)
-    val characterState: StateFlow<UiState<CharacterModel>> = _characterState
+    private val _characterState = MutableStateFlow<ResultState<CharacterModel>>(ResultState.Loading)
+    val characterState: StateFlow<ResultState<CharacterModel>> = _characterState
 
     suspend fun getCharacter(id: String) {
         if (characterId == null) {
-            // Emit loading state initially
-            _characterState.emit(UiState.Loading)
+            getCharacterUseCase.invoke(id).collect { collect ->
+                _characterState.emit(collect)
+                characterId = id
 
-            // Use `getCharacterUseCase` to fetch data from repository
-            getCharacterByIdUseCase.invoke(id)
-                .catch {
-                    // Emit error state when an exception occurs
-                    _characterState.emit(UiState.Error(NetworkException.ClientNetworkException))
-                }
-                .collect { characterModel ->
-                    // On success, emit success state
-                    _characterState.emit(UiState.Success(characterModel))
-                    characterId = id
-                }
+            }
         }
     }
 }
