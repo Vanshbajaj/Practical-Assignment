9ea884a04ee07990d06d0f3cd94f6910537c0d88
vbajaj <vbajaj55@gmail.com>
2024-11-26 12:40:04 +0530

    Added Data Layer And Domain Layer For Screen 3

--- domain/src/main/java/com/practical/domain/repository/CharacterRepository.kt
+++ domain/src/main/java/com/practical/domain/repository/CharacterRepository.kt
@@ -3,11 +3,12 @@ package com.practical.domain.repository
 import com.practical.domain.CharacterModel
 import com.practical.domain.CharactersListModel
 import com.practical.domain.EpisodeModelDetails
+import com.practical.domain.ResultState
 import kotlinx.coroutines.flow.Flow
 
 
 interface CharacterRepository {
-    fun getCharactersList(): Flow<List<CharactersListModel>>
-    fun getCharacter(id: String): Flow<CharacterModel>
-    fun getEpisodeId(id: String): Flow<EpisodeModelDetails>
+    fun getCharactersList(): Flow<ResultState<List<CharactersListModel>>>
+    fun getCharacter(id: String): Flow<ResultState<CharacterModel>>
+    fun getEpisodeId(id: String): Flow<ResultState<EpisodeModelDetails>>
 }
