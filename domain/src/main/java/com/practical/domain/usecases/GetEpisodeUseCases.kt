486cedec6678ef8c8aad18ed5bf65073901ba927
vbajaj <vbajaj55@gmail.com>
2024-11-26 12:40:04 +0530

    Added Data Layer And Domain Layer For Screen 3

--- domain/src/main/java/com/practical/domain/usecases/GetEpisodeUseCases.kt
+++ domain/src/main/java/com/practical/domain/usecases/GetEpisodeUseCases.kt
@@ -1,11 +1,12 @@
 package com.practical.domain.usecases
 
 import com.practical.domain.EpisodeModelDetails
+import com.practical.domain.ResultState
 import com.practical.domain.repository.CharacterRepository
 import kotlinx.coroutines.flow.Flow
 
 class GetEpisodeUseCases(private val repository: CharacterRepository) {
-    operator fun invoke(id: String): Flow<EpisodeModelDetails> {
+    operator fun invoke(id: String): Flow<ResultState<EpisodeModelDetails>> {
         return repository.getEpisodeId(id)
     }
 }
