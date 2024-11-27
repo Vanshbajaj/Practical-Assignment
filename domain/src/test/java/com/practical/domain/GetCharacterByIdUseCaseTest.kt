43165a790b4a16a7e153b866157657bfed327bf3
vbajaj <vbajaj55@gmail.com>
2024-11-27 11:54:54 +0530

    Fixed PR Comments

--- domain/src/test/java/com/practical/domain/GetCharacterByIdUseCaseTest.kt
+++ domain/src/test/java/com/practical/domain/GetCharacterByIdUseCaseTest.kt
@@ -2,58 +2,95 @@ package com.practical.domain
 
 import app.cash.turbine.test
 import com.practical.domain.repository.CharacterRepository
-import com.practical.domain.usecases.GetCharacterByIdUseCase
+import com.practical.domain.usecases.GetCharacterUseCase
 import io.mockk.coEvery
 import io.mockk.mockk
-import kotlinx.coroutines.flow.emptyFlow
-import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.test.runTest
 import org.junit.Assert.assertEquals
-import org.junit.Assert.assertNull
+import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class GetCharacterByIdUseCaseTest {
     private val characterRepository = mockk<CharacterRepository>(relaxed = true)
-    private val getCharacterByIdUseCase = GetCharacterByIdUseCase(characterRepository)
+    private val getCharacterUseCase = GetCharacterUseCase(characterRepository)
+
 
     @Test
-    fun `should return character when repository returns valid data`() = runTest {
+    fun `should return Loading when character is being fetched`() = runTest {
         // Given
-        val characterId = "1"
-        val character = CharacterModel(
-            name = "Character 1",
-            image = "http://example.com/character1.png"
-        )
+        val characterId = "123"
+        coEvery { characterRepository.getCharacter(characterId) } returns flow {
+            emit(ResultState.Loading)
+        }
 
-        // Mock the repository to return the character when getCharacter is called
-        coEvery { characterRepository.getCharacter(characterId) } returns flowOf(character)
-
-        // When & Then
-        getCharacterByIdUseCase(characterId).test {
-            // Collect the emitted character from the flow
-            val emittedCharacter = awaitItem()
-
-            // Assert that the emitted character matches the expected character
-            assertEquals(character, emittedCharacter)
-
-            cancelAndIgnoreRemainingEvents()
+        // When
+        getCharacterUseCase(characterId).test {
+            // Collecting the result emitted by the flow
+            assertTrue(awaitItem() is ResultState.Loading)
+            awaitComplete() // Then
         }
     }
 
     @Test
-    fun `should handle empty flow when no character is found`() = runTest {
+    fun `should return Success when character is found`() = runTest {
         // Given
-        val characterId = "999" // An ID that is unlikely to exist
+        val characterId = "123"
+        val characterModel = CharacterModel(id = characterId, name = "Test Character")
+        coEvery { characterRepository.getCharacter(characterId) } returns flow {
+            emit(ResultState.Loading)
+            emit(ResultState.Success(characterModel))
+        }
 
-        // Mock the repository to return an empty flow when getCharacter is called
-        coEvery { characterRepository.getCharacter(characterId) } returns emptyFlow()
+        // When
+        getCharacterUseCase(characterId).test {
+            // Collecting the result emitted by the flow
+            assertTrue(awaitItem() is ResultState.Loading)
+            assertEquals(characterModel, (awaitItem() as ResultState.Success).data)
+            awaitComplete() // Then
+        }
+    }
 
-        // When & Then
-        getCharacterByIdUseCase(characterId).test {
-            // Collect the result from the flow (which should be empty)
-            assertNull(null)
-            cancelAndIgnoreRemainingEvents()
+    @Test
+    fun `should return Error when character is not found`() = runTest {
+        // Given
+        val characterId = "123"
+        coEvery { characterRepository.getCharacter(characterId) } returns flow {
+            emit(ResultState.Loading)
+            emit(ResultState.Error(RuntimeException("Character not found")))
+        }
 
+        // When
+        getCharacterUseCase(characterId).test {
+            // Collecting the result emitted by the flow
+            assertTrue(awaitItem() is ResultState.Loading)
+            assertEquals(
+                "Character not found",
+                (awaitItem() as ResultState.Error).exception.message
+            )
+            awaitComplete() // Then
+        }
+    }
+
+
+    @Test
+    fun `should return Error when an exception is thrown`() = runTest {
+        // Given
+        val characterId = "123"
+        coEvery { characterRepository.getCharacter(characterId) } returns flow {
+            emit(ResultState.Loading)
+            emit(ResultState.Error(RuntimeException("Something went wrong")))
+        }
+
+        //When
+        getCharacterUseCase(characterId).test {
+            // Collecting the result emitted by the flow
+            assertTrue(awaitItem() is ResultState.Loading)
+            assertEquals(
+                "Something went wrong",
+                (awaitItem() as ResultState.Error).exception.message
+            )
+            awaitComplete() //Then
         }
     }
 }
