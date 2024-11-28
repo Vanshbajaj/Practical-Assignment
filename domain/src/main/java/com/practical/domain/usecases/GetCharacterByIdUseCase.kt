b96c31b513efa01d04d2a3354903ea718c08f995
vbajaj <vbajaj55@gmail.com>
2024-11-28 22:11:10 +0530

    Added Ui State in Presentation Layer

--- domain/src/main/java/com/practical/domain/usecases/GetCharacterByIdUseCase.kt
+++ domain/src/main/java/com/practical/domain/usecases/GetCharacterByIdUseCase.kt
@@ -4,7 +4,7 @@ import com.practical.domain.CharacterModel
 import com.practical.domain.repository.CharacterRepository
 import kotlinx.coroutines.flow.Flow
 
-class GetCharacterByIdUseCase(private val repository: CharacterRepository) {
+class GetCharacterUseCase(private val repository: CharacterRepository) {
      operator fun invoke(id: String): Flow<CharacterModel> {
         return repository.getCharacter(id)
     }
