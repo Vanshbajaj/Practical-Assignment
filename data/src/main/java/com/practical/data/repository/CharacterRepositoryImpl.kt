25672de3d18a0a5151ae0a2faf48b73ef66c6854
vbajaj <vbajaj55@gmail.com>
2024-12-02 12:39:13 +0530

    Fixed PR comments

--- data/src/main/java/com/practical/data/repository/CharacterRepositoryImpl.kt
+++ data/src/main/java/com/practical/data/repository/CharacterRepositoryImpl.kt
@@ -1,17 +1,10 @@
 package com.practical.data.repository
 
 import com.apollographql.apollo.ApolloClient
-import com.apollographql.apollo.exception.ApolloException
-import com.apollographql.apollo.exception.ApolloNetworkException
-import com.apollographql.apollo.exception.DefaultApolloException
-import com.apollographql.apollo.exception.RouterError
-import com.apollographql.apollo.exception.SubscriptionConnectionException
-import com.apollographql.apollo.exception.SubscriptionOperationException
 import com.data.graphql.CharacterDetailsQuery
 import com.data.graphql.CharactersListQuery
 import com.data.graphql.EpisodeQuery
-import com.practical.data.network.ApolloClientException
-import com.practical.data.network.ClientNetworkException
+import com.practical.data.network.NetworkException
 import com.practical.data.toCharacterModel
 import com.practical.data.toEpisodeModelDetails
 import com.practical.domain.CharacterModel
@@ -21,16 +14,15 @@ import com.practical.domain.repository.CharacterRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.catch
 import kotlinx.coroutines.flow.flow
-import java.io.IOException
 import javax.inject.Inject
 
 class CharacterRepositoryImpl @Inject constructor(
     private val apolloClient: ApolloClient,
 ) : CharacterRepository {
 
-    override fun getCharactersList(): Flow<List<CharactersListModel>> = flow {
-        val response = apolloClient.query(CharactersListQuery()).execute()
-        try {
+    override fun getCharactersList(): Flow<List<CharactersListModel>> {
+        return flow {
+            val response = apolloClient.query(CharactersListQuery()).execute()
             val characters = response.data?.characters?.results?.mapNotNull { character ->
                 CharactersListModel(
                     id = character?.id,
@@ -39,27 +31,19 @@ class CharacterRepositoryImpl @Inject constructor(
                 )
             } ?: emptyList()
             emit(characters)
-        } catch (t: ApolloException) {
-            // Handle specific ApolloException cases
-            when (t) {
-                is ApolloNetworkException -> {
-                    throw ClientNetworkException()
+
+        }.catch { throwable ->
+            when (throwable) {
+                is NetworkException.ClientNetworkException -> {
+                    throw NetworkException.ClientNetworkException
                 }
 
-                is SubscriptionOperationException, is SubscriptionConnectionException, is RouterError -> {
-                    throw ApolloClientException()
-                }
-
-                else -> {
-                    // Default case for unknown ApolloExceptions
-                    throw DefaultApolloException("Unknown Apollo error occurred", t)
+                is NetworkException.ApolloClientException -> {
+                    throw NetworkException.ApolloClientException
                 }
             }
-        } catch (e: IOException) {
-            // Handle any other IOExceptions like network disconnections or timeouts
-            throw ApolloNetworkException("General I/O error occurred", e)
-        }
 
+        }
     }
 
     override fun getCharacter(id: String): Flow<CharacterModel> {
@@ -67,23 +51,18 @@ class CharacterRepositoryImpl @Inject constructor(
             val response = apolloClient.query(CharacterDetailsQuery(id)).execute()
             response.data?.character?.toCharacterModel()?.let {
                 emit(it) // Emit character data
-            } ?: throw ApolloClientException() // Throw error if no character found
+            }
+                ?: throw NetworkException.ApolloClientException
         }.catch { throwable ->
-            // Handle and propagate errors from the data layer
             when (throwable) {
-                is ApolloNetworkException -> {
-                    // Propagate ApolloNetworkException to ViewModel
-                    throw ApolloNetworkException("Network error occurred. Please check your internet connection.")
+                is NetworkException.ClientNetworkException -> {
+                    throw NetworkException.ClientNetworkException
                 }
-                is ApolloClientException -> {
-                    throw ApolloClientException()
+
+                is NetworkException.ApolloClientException -> {
+                    throw NetworkException.ApolloClientException
                 }
-                is IOException -> {
-                    throw ApolloNetworkException()
-                }
-                else -> {
-                    throw ApolloClientException()
-                }
+
             }
         }
     }
@@ -91,29 +70,18 @@ class CharacterRepositoryImpl @Inject constructor(
 
     override fun getEpisodeId(id: String): Flow<EpisodeModelDetails> {
         return flow {
-            try {
-                val response = apolloClient.query(EpisodeQuery(id)).execute()
-                response.data?.episode?.toEpisodeModelDetails()?.let { emit(it) }
+            val response = apolloClient.query(EpisodeQuery(id)).execute()
+            response.data?.episode?.toEpisodeModelDetails()?.let { emit(it) }
 
-            } catch (t: ApolloException) {
-                // Handle specific ApolloException cases
-                when (t) {
-                    is ApolloNetworkException -> {
-                        throw ClientNetworkException()
-                    }
-
-                    is SubscriptionOperationException, is SubscriptionConnectionException, is RouterError -> {
-                        throw ApolloClientException()
-                    }
-
-                    else -> {
-                        // Default case for unknown ApolloExceptions
-                        throw DefaultApolloException("Unknown Apollo error occurred", t)
-                    }
+        }.catch { throwable ->
+            when (throwable) {
+                is NetworkException.ClientNetworkException -> {
+                    throw NetworkException.ClientNetworkException
                 }
-            } catch (e: IOException) {
-                // Handle any other IOExceptions like network disconnections or timeouts
-                throw ApolloNetworkException("General I/O error occurred", e)
+
+                is NetworkException.ApolloClientException -> {
+                    throw NetworkException.ApolloClientException
+                }
             }
         }
     }
