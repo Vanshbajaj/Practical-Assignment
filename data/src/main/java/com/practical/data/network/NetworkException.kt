d1778368202a2381ebd0bd650fdf4b6cc01d7ad7
vbajaj <vbajaj55@gmail.com>
2024-12-02 21:09:25 +0530

    Removed Extra Spaces from NetworkException

--- data/src/main/java/com/practical/data/network/NetworkException.kt
+++ data/src/main/java/com/practical/data/network/NetworkException.kt
@@ -2,13 +2,7 @@
 
 package com.practical.data.network
 
-
 sealed class NetworkException : Exception() {
     data object ClientNetworkException : NetworkException()
     data object ApolloClientException : NetworkException()
 }
-
-
-
-
-
