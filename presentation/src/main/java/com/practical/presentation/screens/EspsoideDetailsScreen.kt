fda5c17a3d576e5db5dedce7ec800c284b649f5d
vbajaj <vbajaj55@gmail.com>
2024-12-03 15:40:32 +0530

    Added Assisted Injection For EpisodeScreen Fixed Some Ui Issues for the CharacterScreen

--- presentation/src/main/java/com/practical/presentation/screens/EspsoideDetailsScreen.kt
+++ presentation/src/main/java/com/practical/presentation/screens/EspsoideDetailsScreen.kt
@@ -2,40 +2,16 @@ package com.practical.presentation.screens
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
-import com.practical.data.network.NetworkException
-import com.practical.presentation.UiState
-import com.practical.presentation.ui.theme.dimens
+import androidx.compose.ui.unit.dp
 import com.practical.presentation.viewmodel.EpisodeDetailsViewModel
 
 @Composable
-fun EpisodeDetails(viewModel: EpisodeDetailsViewModel, modifier: Modifier = Modifier) {
+fun EpisodeDetails(viewModel: EpisodeDetailsViewModel,modifier: Modifier) {
     Column(
-        modifier = modifier.padding(MaterialTheme.dimens.paddingExtraSmall)
+        modifier = Modifier.padding(16.dp)
     ) {
-        val episodeState by viewModel.episodeState.collectAsState()
-        when (val state = episodeState) {
-            UiState.Loading -> {
-                CircularProgressIndicator()
-            }
 
-            is UiState.Error -> {
-                when(state.exception){
-                    is NetworkException.ClientNetworkException->{
-
-                    }
-
-                }
-            }
-
-            is UiState.Success -> {
-
-            }
-        }
     }
 }
