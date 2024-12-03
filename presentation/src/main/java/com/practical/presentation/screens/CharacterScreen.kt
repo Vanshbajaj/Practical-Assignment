1b409f27e6f65eca09cf661dcb7a0319309499c5
vbajaj <vbajaj55@gmail.com>
2024-12-03 15:40:32 +0530

    Added Assisted Injection For EpisodeScreen Fixed Some Ui Issues for the CharacterScreen

--- presentation/src/main/java/com/practical/presentation/screens/CharacterScreen.kt
+++ presentation/src/main/java/com/practical/presentation/screens/CharacterScreen.kt
@@ -1,21 +1,17 @@
 package com.practical.presentation.screens
 
 
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyRow
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Card
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.MaterialTheme
@@ -27,7 +23,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -38,8 +33,6 @@ import com.practical.domain.CharacterModel
 import com.practical.domain.EpisodeModel
 import com.practical.presentation.R
 import com.practical.presentation.UiState
-import com.practical.presentation.common.ErrorMessage
-import com.practical.presentation.ui.theme.Purple
 import com.practical.presentation.ui.theme.Purple40
 import com.practical.presentation.ui.theme.dimens
 import com.practical.presentation.viewmodel.CharacterDetailsViewModel
@@ -68,30 +61,48 @@ private fun CharacterScreenContent(
     modifier: Modifier = Modifier,
 ) {
     Box(
-        modifier.fillMaxSize()
+        modifier.fillMaxSize() // Fill the entire screen
     ) {
-        when (state) {
-            is UiState.Loading -> {
-                CircularProgressIndicator(
-                    Modifier.align(Alignment.Center)
-                )
-            }
+        Column {
+            when (state) {
+                is UiState.Loading -> {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        CircularProgressIndicator(
+                            Modifier.align(Alignment.Center)
+                        )
+                    }
+                }
 
-            is UiState.Success -> {
-                // When data is loaded, display the character details
-                TopData(character = state.data, onNavigateToCharacterScreen)
-            }
+                is UiState.Success -> {
+                    // When data is loaded, display the character details
+                    TopData(character = state.data, onNavigateToCharacterScreen)
+                }
 
-            is UiState.Error -> {
-                // Error handling for network failure, etc.
-                ErrorMessage(exception = state.exception, modifier = Modifier.fillMaxSize())
+                is UiState.Error -> {
+                    // Error handling for network failure, etc.
+                    when (state.exception) {
+                        is NetworkException.ClientNetworkException -> {
+                            Box(
+                                modifier = Modifier
+                                    .fillMaxSize()
+                            ) {
+                                Text(
+                                    text = stringResource(R.string.no_internet_data),
+                                    color = MaterialTheme.colorScheme.error,
+                                    style = MaterialTheme.typography.bodyLarge, // Optional: Add style
+                                    modifier = Modifier.align(Alignment.Center) // Ensure it is centered
+                                )
+                            }
+                        }
+                    }
+                }
             }
         }
-
     }
 }
-
-
 
 @Composable
 private fun TopData(
@@ -127,7 +138,7 @@ private fun TopData(
                 modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
                 text = character.status,
                 style = MaterialTheme.typography.titleMedium,
-                color = if (character.status == stringResource(R.string.label_alive)) Purple40 else Color.Red
+                color = if (character.status == "Alive") Purple40 else Color.Red
             )
         }
 
@@ -151,57 +162,19 @@ private fun TopData(
         Row(
             modifier = Modifier
                 .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
+            horizontalArrangement = Arrangement.SpaceBetween
         ) {
             Text(
                 text = stringResource(R.string.gender),
                 style = MaterialTheme.typography.titleMedium
             )
-            Text(
-                modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
-                text = character.gender,
-                style = MaterialTheme.typography.titleMedium, color = Purple40
-        )
-        if (character.gender == stringResource(R.string.txt_male)) {
-                Image(painter = painterResource(R.drawable.ic_male), contentDescription = "")
-            } else {
-                Image(painter = painterResource(R.drawable.ic_female), contentDescription = "")
-            }
-        }
-        Spacer(Modifier.padding(MaterialTheme.dimens.paddingExtraSmall))
-        Text(
-            text = stringResource(R.string.label_origin),
-            fontSize = MaterialTheme.typography.titleLarge.fontSize
-        )
-        Spacer(Modifier.padding(MaterialTheme.dimens.paddingExtraSmall))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Text(
-                text = stringResource(R.string.label_name),
-                style = MaterialTheme.typography.titleMedium
-            )
-            Text(
-                modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
-                text = character.origin.name,
-                style = MaterialTheme.typography.titleMedium,
-                color = Purple40
-            )
-        }
-    Row(
-        modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start
-    ) {
-        Text(
-            text = stringResource(R.string.label_dimension), style = MaterialTheme.typography.titleMedium
-        )
-        Text(
-            modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
-            text = character.origin.dimension.orEmpty(),
-                style = MaterialTheme.typography.titleMedium,
-                color = Purple40
-            )
+//            Icon(
+//                imageVector = if (character.status == "MALE") Icons.Default.M else Icons.Default.Woman,
+//                contentDescription = null,
+//                tint = MaterialTheme.colors.primary
+//            )
+
+
         }
         Text(
             text = stringResource(R.string.episodes),
@@ -230,32 +203,20 @@ private fun EpisodeCard(
         modifier = modifier
             .padding(horizontal = MaterialTheme.dimens.paddingExtraSmall)
             .width(MaterialTheme.dimens.cardWidth)
-            .height(MaterialTheme.dimens.cardWidth)
             .clickable(
-                enabled = episode.id.isNotEmpty()
+                enabled = episode.id
+                    .isEmpty()
+                    .not()
             ) {
                 episode.id.let { onNavigateToCharacterScreen.invoke(it) }
-            },
-        shape = RoundedCornerShape(MaterialTheme.dimens.paddingMedium),
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .background(Purple)
-        ) {
-            Column(
-                verticalArrangement = Arrangement.Center, // Center content vertically
-                horizontalAlignment = Alignment.CenterHorizontally, // Center content horizontally
-                modifier = Modifier.fillMaxSize() // Make Column take up the full size of the Box
-            ) {
-                Text(
-                    text = episode.name,
-                    style = MaterialTheme.typography.bodyLarge, // You can customize this style
-                    color = Color.White, // Set text color to white
-                    modifier =
-                    Modifier.padding(MaterialTheme.dimens.paddingSmall) // Optional padding for better spacing
-                )
+
             }
+    ) {
+        Row(
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.padding(MaterialTheme.dimens.paddingMedium)
+        ) {
+            Text(text = episode.name, fontSize = MaterialTheme.typography.labelLarge.fontSize)
         }
     }
 }
