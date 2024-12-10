6d25b4aa2b1de2b5dc38e3f05d6bca8ed0221156
vbajaj <vbajaj55@gmail.com>
2024-12-10 18:22:02 +0530

    Added Modifier to CharacterScreen

--- presentation/src/main/java/com/practical/presentation/screens/CharacterScreen.kt
+++ presentation/src/main/java/com/practical/presentation/screens/CharacterScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
-import com.practical.data.network.NetworkException
 import com.practical.domain.CharacterModel
 import com.practical.domain.EpisodeModel
 import com.practical.presentation.R
@@ -90,7 +89,6 @@ private fun CharacterScreenContent(
 }
 
 
-
 @Composable
 private fun TopData(
     character: CharacterModel,
@@ -108,7 +106,11 @@ private fun TopData(
 }
 
 @Composable
-private fun CharacterImage(character: CharacterModel, screenHeight: Dp, modifier: Modifier = Modifier) {
+private fun CharacterImage(
+    character: CharacterModel,
+    screenHeight: Dp,
+    modifier: Modifier = Modifier,
+) {
     AsyncImage(
         model = character.image,
         contentDescription = character.name,
@@ -154,7 +156,12 @@ private fun CharacterInfo(character: CharacterModel, modifier: Modifier = Modifi
 
 
 @Composable
-private fun CharacterRow(label: String, value: String, color: Color = Purple40, modifier: Modifier = Modifier) {
+private fun CharacterRow(
+    label: String,
+    value: String,
+    modifier: Modifier = Modifier,
+    color: Color = Purple40,
+) {
     Row(
         modifier = modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start
     ) {
