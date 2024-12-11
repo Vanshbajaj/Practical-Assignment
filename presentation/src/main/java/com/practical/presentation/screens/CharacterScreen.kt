54b116859ebe0d9dbba3a70370469fc4f253310f
vbajaj <vbajaj55@gmail.com>
2024-12-11 19:57:00 +0530

    Fixed the Null Case

--- presentation/src/main/java/com/practical/presentation/screens/CharacterScreen.kt
+++ presentation/src/main/java/com/practical/presentation/screens/CharacterScreen.kt
@@ -1,14 +1,12 @@
 package com.practical.presentation.screens
 
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
@@ -27,9 +25,9 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import coil.compose.AsyncImage
@@ -98,15 +96,8 @@ private fun TopData(
     modifier: Modifier = Modifier,
 ) {
     val screenHeight = LocalConfiguration.current.screenHeightDp.dp
+
     Column(modifier = modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall)) {
-<<<<<<< HEAD
-        AsyncImage(
-            model = character.image,
-            contentDescription = character.name,
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(screenHeight / CharacterScreenValues.SCREEN_HEIGHT_BY_TWO)
-=======
         CharacterImage(character = character, screenHeight = screenHeight)
         Text(character.name, fontSize = MaterialTheme.typography.headlineLarge.fontSize)
         CharacterInfo(character = character)
@@ -137,101 +128,20 @@ private fun CharacterInfo(character: CharacterModel, modifier: Modifier = Modifi
             label = stringResource(R.string.status),
             value = character.status,
             color = if (character.status == "Alive") Purple40 else Color.Red
->>>>>>> 24aae28 (Added Modifier to CharacterScreen)
         )
-
+        CharacterRow(
+            label = stringResource(R.string.species), value = character.species, color = Purple40
+        )
+        CharacterRow(label = stringResource(R.string.gender), value = character.gender)
         Text(
-            text = character.name,
-            fontSize = MaterialTheme.typography.headlineLarge.fontSize
-        )
-
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Text(
-                text = stringResource(R.string.status),
-                style = MaterialTheme.typography.titleMedium
-            )
-            Text(
-                modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
-                text = character.status,
-                style = MaterialTheme.typography.titleMedium,
-                color = if (character.status == stringResource(R.string.label_alive)) Purple40 else Color.Red
-            )
-        }
-
-        // Species Row
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Text(
-                text = stringResource(R.string.species),
-                style = MaterialTheme.typography.titleMedium
-            )
-            Text(
-                modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
-                text = character.species,
-                style = MaterialTheme.typography.titleMedium,
-                color = Purple40
-            )
-        }
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Text(
-                text = stringResource(R.string.gender),
-                style = MaterialTheme.typography.titleMedium
-            )
-            Text(
-                modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
-                text = character.gender,
-                style = MaterialTheme.typography.titleMedium, color = Purple40
-        )
-        if (character.gender == stringResource(R.string.txt_male)) {
-                Image(painter = painterResource(R.drawable.ic_male), contentDescription = "")
-            } else {
-                Image(painter = painterResource(R.drawable.ic_female), contentDescription = "")
-            }
-        }
-        Spacer(Modifier.padding(MaterialTheme.dimens.paddingExtraSmall))
-        Text(
-            text = stringResource(R.string.label_origin),
+            stringResource(R.string.label_origin),
             fontSize = MaterialTheme.typography.titleLarge.fontSize
         )
-        Spacer(Modifier.padding(MaterialTheme.dimens.paddingExtraSmall))
-        Row(
-            modifier = Modifier
-                .fillMaxWidth(),
-            horizontalArrangement = Arrangement.Start
-        ) {
-            Text(
-                text = stringResource(R.string.label_name),
-                style = MaterialTheme.typography.titleMedium
-            )
-            Text(
-                modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
-                text = character.origin.name,
-                style = MaterialTheme.typography.titleMedium,
-                color = Purple40
-            )
-        }
-    Row(
-        modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.Start
-    ) {
-        Text(
-            text = stringResource(R.string.label_dimension), style = MaterialTheme.typography.titleMedium
-        )
-        Text(
-            modifier = Modifier.padding(horizontal = MaterialTheme.dimens.paddingExtraSmall),
-            text = character.origin.dimension.orEmpty(),
-                style = MaterialTheme.typography.titleMedium,
-                color = Purple40
+        CharacterRow(label = stringResource(R.string.label_name), value = character.origin.name)
+        character.origin.dimension?.let {
+            CharacterRow(
+                label = stringResource(R.string.label_dimension),
+                value = it
             )
         }
         Text(
@@ -243,12 +153,6 @@ private fun CharacterInfo(character: CharacterModel, modifier: Modifier = Modifi
                 vertical = MaterialTheme.dimens.paddingSmall
             )
         )
-<<<<<<< HEAD
-        LazyRow(modifier = Modifier.padding(MaterialTheme.dimens.paddingSmall)) {
-            items(character.episodes.size) { episode ->
-                EpisodeCard(episode = character.episodes[episode], onNavigateToCharacterScreen)
-            }
-=======
     }
 }
 
@@ -285,7 +189,6 @@ private fun CharacterEpisodes(
     LazyRow(modifier = modifier.padding(MaterialTheme.dimens.paddingSmall)) {
         items(character.episodes.size) { episode ->
             EpisodeCard(episode = character.episodes[episode], onNavigateToCharacterScreen)
->>>>>>> 24aae28 (Added Modifier to CharacterScreen)
         }
     }
 }
@@ -333,4 +236,3 @@ private fun EpisodeCard(
 internal object CharacterScreenValues {
     const val SCREEN_HEIGHT_BY_TWO = 2
 }
-
